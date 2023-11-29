6b4f6d24745caee4df6a0c5af971a71bad7cdc27
Lars Eckart <lars.eckart@hey.com>
2023-11-29 12:49:01 +0200

    all in with pairs

--- Player.js
+++ Player.js
@@ -1,10 +1,14 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return '0.2';
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    if(gameState.players[gameState.in_action].hole_cards[0].rank === gameState.players[gameState.in_action].hole_cards[1].rank) {
+      bet(1000);
+    } else {
+      bet(0);
+    }
   }
 
   static showdown(gameState) {
